45aff2bca66c1bdba3a3fc6730edb5fba56d0e94
eddy makboul <eddymakboul@icloud.com>
2021-11-24 14:52:40 +0100

    EML - css

--- arch-app/src/main/resources/static/cooptation.js
+++ arch-app/src/main/resources/static/cooptation.js
@@ -11,24 +11,26 @@ const userModel = {
 export const Cooptation = {
     template:
         '<div>\n' +
-        '<form id="app" method="post" novalidate="true">\n' +
+        '<h1 class="d-flex justify-content-center text-dark">Cooptation</h1>\n' +
+        '<div class="border border-secondary rounded p-3">\n' +
+        '<form class="form-center edit-person" id="app" method="post" novalidate="true">\n' +
         '<div class="form-group">\n' +
         '<label>Name :</label>\n' +
-        '    <input v-model="user.nom" class="form-control" />\n' +
+        '    <input type="text" v-model="user.nom" class="form-control" />\n' +
         '<div v-if="(errors?.nom)" class="alert alert-warning">\n' +
         '        {{errors?.nom}}\n' +
         '    </div>\n' +
         '</div>\n' +
         '<div class="form-group">\n' +
         '    <label>firstname :</label>\n' +
-        '    <input v-model="user.firstname" class="form-control"/>' +
+        '    <input type="text" v-model="user.firstname" class="form-control"/>' +
         '    <div v-if="(errors?.firstname)" class="alert alert-warning">\n' +
         '        {{errors.firstname}}\n' +
         '    </div>\n' +
         '</div>\n' +
         '<div class="form-group">\n' +
         '    <label>email :</label>\n' +
-        '    <input v-model="user.email" class="form-control"/>\n' +
+        '    <input type="email" v-model="user.email" class="form-control"/>\n' +
         '    <div v-if="(errors?.email)" class="alert alert-warning">\n' +
         '        {{errors.email}}\n' +
         '    </div>\n' +
@@ -42,25 +44,26 @@ export const Cooptation = {
         '</div>\n' +
         '<div class="form-group">\n' +
         '    <label>password :</label>\n' +
-        '    <input v-model="user.password" class="form-control"/>\n' +
+        '    <input type="password" v-model="user.password" class="form-control"/>\n' +
         '    <div v-if="(errors?.password)" class="alert alert-warning">\n' +
         '        {{errors.password}}\n' +
         '    </div>\n' +
         '</div>\n' +
         '<div class="form-group">\n' +
         '    <label>birthDay :</label>\n' +
-        '    <input v-model="user.birthDay" class="form-control"/>\n' +
+        '    <input type="date" v-model="user.birthDay" class="form-control"/>\n' +
         '    <div v-if="(errors?.birthDay)" class="alert alert-warning">\n' +
         '        {{errors.birthDay}}\n' +
         '    </div>\n' +
         '</div>\n' +
-        '<div class="form-group">\n' +
+        '<div class="form-validator">\n' +
         '    <button v-on:click.prevent="createUser()" class="btn btn-primary">\n' +
         '        Save</button>\n' +
-        '    <button class="btn btn-primary">\n' +
+        '    <button class="btn btn-danger">\n' +
         '        Abort</button>\n' +
         '</div>\n' +
         '</form>\n' +
+        '</div>\n' +
         '</div>',
     data() {
         return {
